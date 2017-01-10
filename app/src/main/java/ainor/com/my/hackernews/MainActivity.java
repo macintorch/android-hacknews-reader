70a6e40bec7328414cf509bbfe49d24abfefe035
Ainor Syahrizal <macintorch@gmail.com>
2017-01-10 16:14:54 +0800

    setup array list arrayadapter and permission

--- app/src/main/java/ainor/com/my/hackernews/MainActivity.java
+++ app/src/main/java/ainor/com/my/hackernews/MainActivity.java
@@ -2,12 +2,26 @@ package ainor.com.my.hackernews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter mArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView newsListView = (ListView) findViewById(R.id.newsListView);
+
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        newsListView.setAdapter(mArrayAdapter);
     }
 }
