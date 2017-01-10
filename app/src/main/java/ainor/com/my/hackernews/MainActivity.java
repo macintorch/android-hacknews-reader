62b1d24f0a71765c4b36c7764510b84bf7b567de
Ainor Syahrizal <macintorch@gmail.com>
2017-01-10 16:54:40 +0800

    get each article id in jason format

--- app/src/main/java/ainor/com/my/hackernews/MainActivity.java
+++ app/src/main/java/ainor/com/my/hackernews/MainActivity.java
@@ -80,8 +80,36 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray jsonArray = new JSONArray(result);
 
-                for (int i = 0; i < jsonArray.length();i++) {
-                    Log.i("JSONItem", jsonArray.getString(i));
+                int numberOfItems = 20;
+
+                if (jsonArray.length() < 20 ) {
+                    numberOfItems = jsonArray.length();
+                }
+
+                for (int i = 0; i < numberOfItems;i++) {
+
+                    String articleId = jsonArray.getString(i);
+
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/"+ articleId+".json?print=pretty");
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    in = urlConnection.getInputStream();
+
+                    reader = new InputStreamReader(in);
+
+                    data = reader.read();
+
+                    String articleInfo = "";
+
+                    while (data != -1 ) {
+                        char current = (char) data;
+
+                        articleInfo += current;
+
+                        data = reader.read();
+                    }
+
+                    Log.i("ArticleInfo", articleInfo);
                 }
 
             } catch (MalformedURLException e) {
